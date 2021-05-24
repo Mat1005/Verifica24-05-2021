a7bccf67533bde8322b3c68f01faf97be2749be3
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-05-24 12:42:29 +0200

    Add files via upload

---
+++ verificaMatteoZaccarelli.py
@@ -0,0 +1,36 @@
+from spalla import Verifica 
+    
+Verifica.firma("Matteo Zaccarelli") 
+
+es = Verifica.inizia_esercizio(2)
+
+print(es)
+print(es.dati)
+'''
+n1 = 292
+
+lista = [n1]
+lista2 = []
+for n in range(28, n1):
+    n1 =  n1 - 1
+    lista.append(n1)
+for n in lista:
+    if n%4 != 0:
+        lista2.append(n)
+print(lista2)
+es.consegna(lista2)
+'''
+
+lista = ['broke', 'list', 'label', 'thing', 'twelve', 'complex', 'curve', 'weigh', 'favorite', 'graph', 'calm', 'adult', 'built', 'laid', 'image', 'lying', 'calm', 'extra', 'once', 'case', 'among', 'serve', 'trail', 'rose', 'our', 'own', 'pupil', 'missing', 'point', 'stared', 'accident', 'few', 'greatly', 'could', 'offer', 'exist', 'desk', 'review', 'typical', 'television', 'lift', 'bite', 'ago', 'look', 'eye', 'community', 'twenty', 'world', 'drawn', 'tonight', 'create', 'face', 'thousand', 'afraid', 'noted', 'pie', 'center', 'let', 'musical', 'thought', 'held', 'doctor', 'separate', 'planning', 'empty', 'newspaper', 'draw', 'massage', 'discover', 'hit', 'mistake', 'fierce', 'rocket', 'joined', 'boat', 'city', 'living', 'jump', 'without', 'plenty', 'desk', 'teeth', 'term', 'why', 'change', 'consider', 'hope', 'note', 'sort', 'frog', 'exist', 'while', 'giving', 'secret', 'win', 'range', 'thumb', 'airplane', 'lovely', 'reason', 'last', 'color', 'brush', 'tired', 'farmer', 'grain', 'two', 'sell', 'officer', 'safety', 'horn', 'expression', 'taken', 'simple', 'highest', 'yard', 'single', 'drove', 'cream', 'adjective', 'immediately', 'love', 'come', 'important', 'recognize', 'no', 'mouth', 'ate', 'children', 'leaf', 'win', 'tried', 'negative', 'curve', 'thank', 'movie', 'year', 'allow', 'valley', 'fast', 'bark', 'generally', 'wife', 'badly', 'facing', 'progress', 'word', 'happy', 'why', 'tank', 'horse', 'muscle', 'taken', 'themselves', 'list', 'merely', 'soap', 'harder', 'rich', 'heart', 'solve', 'should', 'pressure', 'ask', 'shelf', 'wave', 'call', 'sky', 'some', 'kill', 'child', 'exclaimed', 'nest', 'shorter', 'particles', 'yard', 'stiff', 'arrow', 'related', 'captured', 'central', 'daughter', 'harbor', 'grown', 'single', 'folks', 'younger', 'paid', 'those', 'already', 'fourth', 'send', 'individual', 'combination', 'grandmother', 'steel', 'fire', 'black', 'list', 'label', 'practice', 'directly', 'courage', 'sing', 'progress', 'duty', 'yourself', 'also', 'signal', 'equator', 'loss', 'dance', 'lost', 'partly', 'myself', 'carry', 'chamber', 'next', 'contain', 'signal', 'example', 'require', 'play', 'silent', 'clock', 'gently', 'sentence', 'music', 'printed', 'bigger', 'meat', 'roar', 'rubber', 'pig', 'hungry', 'had', 'finger', 'breeze', 'divide', 'vertical', 'our', 'born', 'poet', 'pride', 'got', 'shadow', 'cutting', 'dish', 'seldom', 'hello', 'enjoy', 'gun', 'direction', 'score', 'single', 'swung', 'coat', 'goes', 'mixture', 'orange', 'eventually', 'clean', 'history', 'collect', 'bat', 'detail', 'joined', 'popular', 'detail', 'factor', 'path', 'word', 'eventually', 'new', 'family', 'iron', 'copper', 'occur', 'maybe', 'mind', 'needed', 'hill', 'enjoy', 'wheel', 'bigger', 'develop', 'wrote', 'managed', 
+'eager', 'outside', 'win', 'loose', 'care', 'minerals', 'factory', 'treated', 'direct', 'include', 'sunlight', 'south', 'ice', 'is', 'hard', 'fence', 'center', 'experiment', 'advice', 'favorite', 'mind', 'classroom', 'experiment', 'center', 'mathematics', 'scientific', 'shape', 'or', 'ruler', 'bone', 'picture', 'strange', 'room', 'find', 'white', 'speech', 'slight', 'carried', 'pipe', 'feet', 'among', 'evening', 'sort', 'solution', 'strong', 'minerals', 'observe', 'rice', 'chamber', 'scared', 'bill', 'hurt', 'strange', 'world', 'owner', 'limited', 'meet', 'lungs', 'knowledge', 'town', 'spread', 'hair', 'particularly', 'orbit', 'brass', 
+'running', 'shells', 'flame', 'shall', 'friend', 'mental', 'dried', 'get', 'burn', 'cover', 'silk', 'even', 'experiment', 'mass', 'noun', 'fifty', 'vapor', 'brass', 'dropped', 'swimming', 'build', 'dug', 'wise', 'board', 'spider', 'mill', 'bowl', 'too', 'least', 'opposite', 'farmer', 'collect', 'must', 'donkey', 'round', 'ship', 'policeman', 'army', 'curve', 'silence', 'gate', 'himself', 'zulu', 'congress', 'dull', 'people', 'also', 'storm', 'brown', 'tent', 'sat', 'bottle', 'leader', 'opinion', 'while', 'race', 'shoot', 'most', 'atmosphere', 'felt', 'around', 'corner', 'path', 'tide', 'came', 'canal', 'parent', 'operation', 'greatest', 'that', 'unknown', 'ever', 'actually', 'different', 'language', 'mix', 'hide', 'high', 'folks', 'region', 'observe', 'serve', 'copper', 'height', 'main', 'sleep', 'per', 'discussion', 'careful', 'diagram', 'mix', 'shape', 'negative', 'shop', 'chosen', 'cold', 'ought', 'particularly', 'lion', 'college', 'train', 'satisfied', 'water', 'touch', 'proper', 'earth', 'customs', 'even', 'fighting', 'him', 'in', 'also', 'in', 'total', 'write', 'butter', 'deeply', 'outside', 'facing', 'song', 'orbit', 'ocean', 'experience', 'lack', 'all', 'roar', 'brief', 'feathers', 'warn', 'review', 'diameter', 'truck', 'join', 'spring', 'rice', 'figure', 'many', 'positive', 'decide', 'please', 'along', 'itself', 'branch', 'driven', 'negative', 'ability', 'smooth']
+lista2 = []
+
+for e in lista:
+    if len(e) < 5:
+        lista2.append(e)
+        
+print(lista2)
+es.consegna(len(lista2))
+
+Verifica.stampa_voto() # stampa il vostro voto attuale
